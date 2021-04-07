c69f93081c769c07a8742628f34304a29d4b2e0d
Juan Pleitez <juanpc13lolol@gmail.com>
2021-04-07 01:24:07 -0600

    agregando tercer nodo etcd

--- local-etcd/comandos.sh
+++ local-etcd/comandos.sh
@@ -1,8 +1,19 @@
 #Datos Generales
 #IPV4
 atol=10.128.0.2
+#atol=34.68.172.16
 torreja=10.128.0.3
-cluster="http://$atol:2379,http://$torreja:2379"
+#torreja=34.72.150.183
+vm=10.128.0.4
+#vm=34.68.214.22
+current_ip=$atol
+
+cluster="atol=http://$atol:2380,torreja=http://$torreja:2380,vm=http://$vm:2380"
+
+etcd_cmd="etcd --name cluster --initial-advertise-peer-urls http://$current_ip:2380 \
+  --listen-peer-urls http://$current_ip:2380 --listen-client-urls http://$current_ip:2379,http://127.0.0.1:2379 
+  --advertise-client-urls http://$current_ip:2379 --initial-cluster-token etcd-cluster-1 \
+  --initial-cluster $cluster --initial-cluster-state new"
 
 #ETCD
 export ETCDCTL_API=3
@@ -27,4 +38,27 @@ etcdctl get torreja_db --print-value-only | base64 --decode
 
 #Correr cluster
 etcd --listen-client-urls="$cluster" --advertise-client-urls="$cluster"
+#atol
+etcd --name atol --initial-advertise-peer-urls http://$atol:2380 \
+  --listen-peer-urls http://$atol:2380 --listen-client-urls http://$atol:2379,http://127.0.0.1:2379 \
+  --advertise-client-urls http://$atol:2379 --initial-cluster-token etcd-cluster-1 \
+  --initial-cluster atol=http://$atol:2380,torreja=http://$torreja:2380 --initial-cluster-state new
+#torreja
+etcd --name torreja --initial-advertise-peer-urls http://$torreja:2380 \
+  --listen-peer-urls http://$torreja:2380 --listen-client-urls http://$torreja:2379,http://127.0.0.1:2379 \
+  --advertise-client-urls http://$torreja:2379 --initial-cluster-token etcd-cluster-1 \
+  --initial-cluster atol=http://$atol:2380,torreja=http://$torreja:2380 --initial-cluster-state new
 
+
+# Correr los contenedores con las variables de entorno
+# atol
+docker build -t atol:1.0 .
+docker run --name atol --hostname atol -e etcd_cmd="$etcd_cmd" \
+--publish 53:53/udp --publish 53:53/tcp --publish 2379:2379/tcp --publish 2380:2380/tcp \
+--rm -it atol:1.0
+
+# torreja
+docker build -t torreja:1.0 .
+docker run --name torreja --hostname torreja -e etcd_cmd="$etcd_cmd" \
+--publish 53:53/udp --publish 53:53/tcp --publish 2379:2379/tcp \
+--rm -it torreja:1.0
