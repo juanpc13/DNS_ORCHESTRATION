d943df41a832cfe021ecc69a621d11b661104874
Juan Pleitez <juanpc13lolol@gmail.com>
2021-04-09 16:39:21 -0600

    Agregando consul

--- comandos.sh
+++ comandos.sh
@@ -2,67 +2,32 @@
 docker network create --subnet 172.20.0.0/16 red-dns
 docker network inspect red-dns
 
-#Datos Generales
-#IPV4
-atol=192.168.2.76
-#atol=172.20.0.2
-#atol=34.68.172.16
-torreja=172.20.0.3
-#torreja=34.72.150.183
-vm=192.168.2.1
-#vm=172.20.0.4
-#vm=34.68.214.22
-
-#Comandos para agreagar nodo
-etcdctl member list
-etcdctl member add atol --peer-urls=http://$atol:2380
-etcdctl member add torreja --peer-urls=http://$torreja:2380
+#Datos Generales, Variables de Entorno
+export VM_IP=172.20.0.10
+export ATOL_IP=172.20.0.20
+export TORREJA_IP=172.20.0.30 #IMPLEMENTAR LUEGO
+export CLUSTER_JOIN="\"$VM_IP\", \"$ATOL_IP\""
+# export CURRENT_IP=$VM_IP #SE ESPECIFICA EN EL DOCKER RUN
 
 #Correr el nodo vm
 # vm
 docker rmi vm:1.0 .
 docker build -t vm:1.0 .
-current_ip=$vm
-cluster="atol=http://$atol:2380,vm=http://$vm:2380"
-etcd_cmd="etcd --name atol --initial-advertise-peer-urls http://$current_ip:2380 \
-  --listen-peer-urls http://$current_ip:2380 \
-  --listen-client-urls http://$current_ip:2379,http://localhost:2379 \
-  --advertise-client-urls http://$current_ip:2379 \
-  --initial-cluster-token etcd-cluster-1 \
-  --initial-cluster $cluster \
-  --initial-cluster-state new"
-docker run --name vm --hostname vm -e etcd_cmd="$etcd_cmd" --net red-dns --ip $vm \
--v "$PWD"/db/:/root/db/ -v "$PWD"/local/:/root/local/ --rm -it vm:1.0
+docker run --name vm --hostname vm --net red-dns --ip $VM_IP -v "$PWD":/root/db -v "$PWD":/root/local \
+-e VM_IP="$VM_IP" -e ATOL_IP="$ATOL_IP" -e CLUSTER_JOIN="$CLUSTER_JOIN" -e CURRENT_IP="$VM_IP" \
+--rm -it vm:1.0
 
 # Correr los contenedores con las variables de entorno
 # atol
 docker rmi atol:1.0 .
 docker build -t atol:1.0 .
-current_ip=$atol
-cluster="atol=http://$atol:2380,vm=http://$vm:2380"
-etcd_cmd="etcd --name atol --initial-advertise-peer-urls http://$current_ip:2380 \
-  --listen-peer-urls http://$current_ip:2380 \
-  --listen-client-urls http://$current_ip:2379,http://localhost:2379 \
-  --advertise-client-urls http://$current_ip:2379 \
-  --initial-cluster-token etcd-cluster-1 \
-  --initial-cluster $cluster \
-  --initial-cluster-state new"
-docker run --name atol --hostname atol -e etcd_cmd="$etcd_cmd" --net red-dns --ip $atol \
+docker run --name atol --hostname atol --net red-dns --ip $atol \
 --publish 53:53/udp --publish 53:53/tcp \
 --rm -it atol:1.0
 
 # torreja
 docker rmi torreja:1.0 .
 docker build -t torreja:1.0 .
-current_ip=$torreja
-cluster="atol=http://$atol:2380,torreja=http://$torreja:2380,vm=http://$vm:2380"
-etcd_cmd="etcd --name atol --initial-advertise-peer-urls http://$current_ip:2380 \
-  --listen-peer-urls http://$current_ip:2380 \
-  --listen-client-urls http://$current_ip:2379,http://127.0.0.1:2379 \
-  --advertise-client-urls http://$current_ip:2379 \
-  --initial-cluster-token etcd-cluster-1 \
-  --initial-cluster $cluster \
-  --initial-cluster-state new"
-docker run --name torreja --hostname torreja -e etcd_cmd="$etcd_cmd" --net red-dns --ip $torreja \
+docker run --name torreja --hostname torreja --net red-dns --ip $torreja \
 --publish 54:53/udp --publish 54:53/tcp \
 --rm -it torreja:1.0
---
+++ vm/db.atol.com
@@ -0,0 +1,20 @@
+; Datos Generales de la ZONA
+$TTL    86400
+
+; Record Inicio de la Autoridad de la ZONA
+@       IN      SOA     ns1.atol.com. jcpleitez.atol.com. (
+                              1         ; Serial
+                         604800         ; Refresh
+                          86400         ; Retry
+                        2419200         ; Expire
+                          86400 )       ; Negative Cache TTL
+;
+
+; Name Servers para el dominio
+@       IN      NS      ns1.atol.com.
+
+; Los registros para direcciones
+ns1     IN      A       190.87.1.130
+www     IN      A       190.87.1.130
+
+
