4590e00b750b7f7cc488852e189d21071bfaecbb
Juan Pleitez <juanpc13lolol@gmail.com>
2021-04-10 18:13:20 +0000

    Agregando a Torreja al Cluster

--- comandos.sh
+++ comandos.sh
@@ -6,7 +6,7 @@ docker network inspect red-dns
 export VM_IP=172.20.0.10
 export ATOL_IP=172.20.0.20
 export TORREJA_IP=172.20.0.30 #IMPLEMENTAR LUEGO
-export CLUSTER_JOIN="\"$VM_IP\", \"$ATOL_IP\""
+export CLUSTER_JOIN="\"$VM_IP\", \"$ATOL_IP\", \"$TORREJA_IP\""
 # export CURRENT_IP=$VM_IP #SE ESPECIFICA EN EL DOCKER RUN
 
 #Correr el nodo vm
@@ -14,7 +14,7 @@ export CLUSTER_JOIN="\"$VM_IP\", \"$ATOL_IP\""
 docker rmi vm:1.0 .
 docker build -t vm:1.0 .
 docker run --name vm --hostname vm --net red-dns --ip $VM_IP -v "$PWD"/files:/root/files \
--e VM_IP="$VM_IP" -e ATOL_IP="$ATOL_IP" -e CLUSTER_JOIN="$CLUSTER_JOIN" -e CURRENT_IP="$VM_IP" \
+-e CURRENT_IP="$VM_IP" -e CLUSTER_JOIN="$CLUSTER_JOIN" \
 --publish 8500:8500 --rm -it vm:1.0
 
 # Correr los contenedores con las variables de entorno
@@ -22,12 +22,12 @@ docker run --name vm --hostname vm --net red-dns --ip $VM_IP -v "$PWD"/files:/ro
 docker rmi atol:1.0 .
 docker build -t atol:1.0 .
 docker run --name atol --hostname atol --net red-dns --ip $ATOL_IP \
--e VM_IP="$VM_IP" -e ATOL_IP="$ATOL_IP" -e CLUSTER_JOIN="$CLUSTER_JOIN" -e CURRENT_IP="$ATOL_IP" \
+-e CURRENT_IP="$ATOL_IP" -e CLUSTER_JOIN="$CLUSTER_JOIN" \
 --publish 53:53/udp --publish 53:53/tcp --rm -it atol:1.0
 
 # torreja
 docker rmi torreja:1.0 .
 docker build -t torreja:1.0 .
-docker run --name torreja --hostname torreja --net red-dns --ip $torreja \
---publish 54:53/udp --publish 54:53/tcp \
---rm -it torreja:1.0
+docker run --name torreja --hostname torreja --net red-dns --ip $TORREJA_IP \
+-e CURRENT_IP="$TORREJA_IP" -e CLUSTER_JOIN="$CLUSTER_JOIN" \
+--publish 54:53/udp --publish 54:53/tcp --rm -it torreja:1.0
